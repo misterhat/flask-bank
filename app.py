00d8993c7c488369584cd10a20174466d0f69b7d
Mister Hat <misterhat144@gmail.com>
2021-08-10 03:34:41 -0500

    add transfers

--- app.py
+++ app.py
@@ -44,13 +44,16 @@ balance_stmt = "SELECT `balance` FROM `bank_users` WHERE `id` = %s"
 transfers_get_stmt = """SELECT `bank_transfers`.*, `bank_users`.`username` AS
 `to_username` FROM
 `bank_transfers` LEFT JOIN `bank_users` ON `bank_users`.`id` =
-`bank_transfers`.`to_user_id` WHERE `user_id` = %s ORDER BY `date` DESC"""
+`bank_transfers`.`to_user_id` WHERE `user_id` = %s OR `to_user_id` = %s
+ORDER BY `date` DESC"""
 
-withdraw_user_stmt = """UPDATE `bank_users` SET `balance` = `balance` - %s
+withdraw_stmt = """UPDATE `bank_users` SET `balance` = `balance` - %s
 WHERE `id` = %s"""
 
-deposit_user_stmt = """UPDATE `bank_users` SET `balance` = `balance` + %s
-WHERE `id` = %s """
+deposit_stmt = """UPDATE `bank_users` SET `balance` = `balance` + %s
+WHERE `id` = %s"""
+
+user_id_stmt = "SELECT `id` FROM `bank_users` WHERE `username` = %s"
 
 transfers_add_stmt = """INSERT INTO `bank_transfers` (`user_id`,
 `amount`, `balance`, `to_user_id`, `reason`, `type`, `date`) VALUES
@@ -59,9 +62,14 @@ transfers_add_stmt = """INSERT INTO `bank_transfers` (`user_id`,
 # use this to create new passwords
 def hash_password(plain):
     salt = os.urandom(64)
-    hashed = scrypt(password, salt=salt, n=2, r=1, p=1)
+    hashed = scrypt(bytes(plain, "utf8"), salt=salt, n=2, r=1, p=1)
+    salt = base64.b64encode(salt).decode("utf8")
+    hashed = base64.b64encode(hashed).decode("utf8")
 
     return (salt, hashed)
+
+#print("password 'test':")
+#print(hash_password("test"))
 
 def format_currency(amount):
     return locale.currency(amount, grouping=True)
@@ -95,16 +103,30 @@ def home():
     if "user" not in session:
         return redirect("/login", 302)
 
+    user_id = session["user"]["id"]
     activities = []
 
-    res = cursor.execute(transfers_get_stmt, session["user"]["id"])
+    res = cursor.execute(transfers_get_stmt, (user_id, user_id))
     res = cursor.fetchall()
 
     for row in res:
         type = format_type(row[6])
         amount = format_currency(row[2])
+        to_user_id = row[4]
+        reason = row[5]
+        to_username = row[8]
+        transfer_from_us = row[6] == TRANSFER and to_user_id != user_id
+        transfer_to_us = row[6] == TRANSFER and to_user_id == user_id
 
-        if row[6] == WITHDRAW:
+        if row[6] == TRANSFER and not reason :
+            reason = "empty reason"
+
+        if transfer_from_us:
+            type = type + " to " + to_username + " (" + reason + ")"
+        elif transfer_to_us:
+            type = type + " from " + to_username + " (" + reason + ")"
+
+        if row[6] == WITHDRAW or transfer_from_us:
             amount = "-" + amount
 
         activities.append({
@@ -114,7 +136,7 @@ def home():
             "date": format_date(row[7])
         })
 
-    balance = format_currency(get_balance(session["user"]["id"]))
+    balance = format_currency(get_balance(user_id))
 
     return render_template(
         "home.html",
@@ -167,30 +189,25 @@ def withdraw():
     if "user" not in session:
         return redirect("/login", 302)
 
+    user_id = session["user"]["id"]
     message = None
     message_type = "danger"
-    balance = get_balance(session["user"]["id"])
+    balance = get_balance(user_id)
 
     if request.method == "POST":
         withdraw_req = float(request.form["amount"])
 
         if (withdraw_req > balance):
-            message = "You don't have that much money!"
+            message = "You don't have that much money."
         else:
-            balance = withdraw_req
+            balance -= withdraw_req
 
-            cursor.execute(
-                withdraw_user_stmt,
-                (
-                    withdraw_req,
-                    session["user"]["id"]
-                )
-            )
+            cursor.execute(withdraw_stmt, (withdraw_req, user_id))
 
             cursor.execute(
                 transfers_add_stmt,
                 (
-                    session["user"]["id"],
+                    user_id,
                     withdraw_req,
                     balance,
                     None,
@@ -221,6 +238,7 @@ def deposit():
     if "user" not in session:
         return redirect("/login", 302)
 
+    user_id = session["user"]["id"]
     message = None
     balance = get_balance(session["user"]["id"])
 
@@ -228,17 +246,12 @@ def deposit():
         deposit_req = float(request.form["amount"])
         balance += deposit_req
 
-        cursor.execute(deposit_user_stmt,
-            (
-                deposit_req,
-                session["user"]["id"]
-            )
-        )
+        cursor.execute(deposit_stmt, (deposit_req, user_id))
 
         cursor.execute(
             transfers_add_stmt,
             (
-                session["user"]["id"],
+                user_id,
                 deposit_req,
                 balance,
                 None,
@@ -261,17 +274,67 @@ def deposit():
         message=message
     )
 
-@app.route("/transfer")
+@app.route("/transfer", methods=["POST", "GET"])
 def transfer():
     if "user" not in session:
         return redirect("/login", 302)
 
-    balance = get_balance(session["user"]["id"])
+    user_id = session["user"]["id"]
+    message = None
+    message_type = "danger"
+    balance = get_balance(user_id)
+
+    if request.method == "POST":
+        user_req = request.form["username"].strip().lower()
+
+        if user_req == session["user"]["username"].lower():
+            message = "You cannot transfer money to yourself."
+        else:
+            transfer_req = float(request.form["amount"])
+
+            if (transfer_req > balance):
+                message = "You don't have that much money."
+            else:
+                cursor.execute(user_id_stmt, request.form["username"])
+                res = cursor.fetchone()
+
+                if not res:
+                    message = "User '" + user_req + "' does not exist."
+                else:
+                    balance -= transfer_req
+
+                    to_user_id = res[0]
+
+                    cursor.execute(withdraw_stmt, (transfer_req, user_id))
+                    cursor.execute(deposit_stmt, (transfer_req, to_user_id))
+
+                    cursor.execute(
+                        transfers_add_stmt,
+                        (
+                            user_id,
+                            transfer_req,
+                            balance,
+                            to_user_id,
+                            request.form["reason"].strip(),
+                            TRANSFER,
+                            int(time.time())
+                        )
+                    )
+
+                    message_type = "success"
+
+                    message = (
+                        "Successfully transferred " +
+                        format_currency(transfer_req) +
+                        " to " + user_req
+                    )
 
     return render_template(
         "transfer.html",
         page="Transfer",
         balance=balance,
         balance_formatted=format_currency(balance),
-        user=session["user"]
+        user=session["user"],
+        message_type=message_type,
+        message=message
     )
