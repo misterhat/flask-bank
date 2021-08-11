a844319bf25f8e11caad33800b149ed108a65698
Mister Hat <misterhat144@gmail.com>
2021-08-11 01:13:05 -0500

    update sql

--- bank.sql
+++ bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 10, 2021 at 04:51 AM
+-- Generation Time: Aug 11, 2021 at 01:12 AM
 -- Server version: 10.5.11-MariaDB-1
 -- PHP Version: 7.4.21
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `bank`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank_log`
+--
+
+CREATE TABLE `bank_log` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `type` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
+  `date` int(10) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -50,20 +63,28 @@ CREATE TABLE `bank_users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `salt` varchar(88) NOT NULL,
-  `balance` double UNSIGNED NOT NULL DEFAULT 0
+  `balance` double UNSIGNED NOT NULL DEFAULT 0,
+  `last_activity` int(10) UNSIGNED NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `bank_users`
 --
 
-INSERT INTO `bank_users` (`id`, `username`, `password`, `salt`, `balance`) VALUES
-(1, 'test', '7qnLcWebmLI2y9rDz3aat8K81WSZAXHQkcFXboHRu+06g6Y5E51CbwaMSFCP+t2gjWiyRPhS2w22mvyUgyz84g==', '5zjIVwhnQkU5yl2w0snzaKncrbtJgm588xmkBV3dT5ZREHvcW6y/F3HQ12J26pFT6CLl7hC02qRa0deeybH/8w==', 58.25),
-(2, 'test2', '7qnLcWebmLI2y9rDz3aat8K81WSZAXHQkcFXboHRu+06g6Y5E51CbwaMSFCP+t2gjWiyRPhS2w22mvyUgyz84g==', '5zjIVwhnQkU5yl2w0snzaKncrbtJgm588xmkBV3dT5ZREHvcW6y/F3HQ12J26pFT6CLl7hC02qRa0deeybH/8w==', 51);
+INSERT INTO `bank_users` (`id`, `username`, `password`, `salt`, `balance`, `last_activity`) VALUES
+(1, 'test', '7qnLcWebmLI2y9rDz3aat8K81WSZAXHQkcFXboHRu+06g6Y5E51CbwaMSFCP+t2gjWiyRPhS2w22mvyUgyz84g==', '5zjIVwhnQkU5yl2w0snzaKncrbtJgm588xmkBV3dT5ZREHvcW6y/F3HQ12J26pFT6CLl7hC02qRa0deeybH/8w==', 100, 0),
+(2, 'test2', '7qnLcWebmLI2y9rDz3aat8K81WSZAXHQkcFXboHRu+06g6Y5E51CbwaMSFCP+t2gjWiyRPhS2w22mvyUgyz84g==', '5zjIVwhnQkU5yl2w0snzaKncrbtJgm588xmkBV3dT5ZREHvcW6y/F3HQ12J26pFT6CLl7hC02qRa0deeybH/8w==', 0, 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bank_log`
+--
+ALTER TABLE `bank_log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `bank_transfers`
@@ -82,6 +103,12 @@ ALTER TABLE `bank_users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `bank_log`
+--
+ALTER TABLE `bank_log`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `bank_transfers`
